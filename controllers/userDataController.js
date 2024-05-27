ec2f12861e1403372744b20fbf91fa15fb23617c
MaheshS03 <maheshraina375@gmail.com>
2024-05-27 19:39:59 +0530

    Initial Commit

---
+++ controllers/userDataController.js
@@ -0,0 +1,25 @@
+const userModel = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const JWT_TOKEN = 'ashbkjcxhuiaklworuybsskhuie'
+
+const displayUserData = async(request, response) => {
+    const {token} = request.body
+    try
+    {
+        const loggedInUser = jwt.verify(token, JWT_TOKEN)
+        const loggedInUserEmail = loggedInUser.email
+        const authenticatedUser = await userModel.findOne({email : loggedInUserEmail})
+        if(authenticatedUser)
+        {
+            return response.status(200).json(authenticatedUser)
+        }
+        response.status(400).send({message : 'Something went wrong..Try again later..'})
+    }
+    catch(error)
+    {
+        response.status(500).send({message: error.message})
+    }
+}
+
+module.exports = {displayUserData}
